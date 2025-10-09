ce031c0ab441479f787f2311cd77627742990fa2
EdierMay <e.g.edier.may@gmail.com>
2025-10-09 17:16:21 -0600

    fixing errors

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-Route::redirect('/', 'admin');
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
+// La ruta a la que deseas redirigir la pagina de inicio
+// Route::redirect('/', 'admin');
+Route::get('/', function () {
+   return view('welcome');
+});
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
+   'auth:sanctum',
+   config('jetstream.auth_session'),
+   'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+   Route::get('/dashboard', function () {
+       return view('dashboard');
+   })->name('dashboard');
 });
